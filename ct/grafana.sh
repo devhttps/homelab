77030206810c3f7bea6703efc6c4caca4ff79ea1
Guilherme Gomes <joe.chips@gmail.com>
2024-08-01 21:35:21 -0300

    Update grafana.sh

--- ct/grafana.sh
+++ ct/grafana.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://github.com/devhttps/homelab/blob/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/devhttps/homelab/main/misc/build.func)
 # Copyright (c) 2024 devhttps
 # Author: Guilherme (devhttps)
 # License: MIT
